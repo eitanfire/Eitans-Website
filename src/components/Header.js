bb7b42d6ccd8dd897b8493110aa8ecb1382bc0a5
Eitan Fire <eitanfire@gmail.com>
2022-10-08 23:14:42 -0600

    Fix broken link on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,20 +3,23 @@ import '../App.css';
 import { Col, Row, Container } from 'reactstrap';
 import Banner from '../app/img/class-website-banner.png';
 import Logo from '../app/img/logo.jpg';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div className='App' id='Header'>
-            <Container>
-                <Row id='banner'>
-                    <Col className="col col-md-4 float-start">
-                        <img id='classWithEitan' src={Banner} alt='Banner' />
-                    </Col>
-                    <Col className="d-none d-lg-block">
-                        <img id="logo" src={Logo} alt='September School logo' />
-                    </Col>
-                </Row>
-            </Container>
+            <Link to='/'>
+                <Container>
+                    <Row id='banner'>
+                        <Col className="col col-md-4 float-start">
+                            <img id='classWithEitan' src={Banner} alt='Banner' />
+                        </Col>
+                        <Col className="d-none d-lg-block">
+                            <img id="logo" src={Logo} alt='September School logo' />
+                        </Col>
+                    </Row>
+                </Container>
+            </Link>
         </div>
     );
 }
