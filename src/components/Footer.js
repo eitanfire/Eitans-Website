7b93deb66ff00ed0426bdcfd0e13bd752facee2b
Eitan Fire <eitanfire@gmail.com>
2022-10-11 01:54:25 -0600

    restyle fonts in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,9 @@ import '../App.css';
 
 const Footer = () => {
     return (
-<p className='footer'><span style={{color: 'transparent'}}><br></br>Eitan FireEFEF</span><span style={{color: 'white'}}>©2022</span> <span >Eitan Fire ⎸<span id='shine'> 
-    A<span> 🌍 </span> 
+<p className='footer'><span style={{color: 'transparent'}}><br></br>Eitan FireEFEF</span><span style={{color: 'white'}}>©2022&nbsp;
+</span> <span style={{color: '#FFFFDB'}}>Eitan Fire<span style={{color: '#00008B'}}> ⎸</span><span id='shine'> 
+&nbsp;A<span> 🌍 </span> 
     <span >Class Experience!</span>
     <span style={{color: 'transparent'}}></span></span></span><br></br><span style={{color: 'transparent'}}>Eitan Fire</span></p>
     );
