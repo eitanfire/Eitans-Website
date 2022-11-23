f3e8f4495917bab9e63258defe2ba0aefeb0a7f6
Eitan Fire <eitanfire@gmail.com>
2022-11-23 00:51:35 -0700

    add react-spring to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import '../App.css';
 import { Col, Row, Container } from 'reactstrap';
 import Banner from '../app/img/class-website-banner.png';
 import Logo from '../app/img/logo.jpg';
 import { Link } from 'react-router-dom';
+import { useSpring, animated, easings } from 'react-spring';
 
 const Header = () => {
+    const [toggle, setToggle] = useState(false)
+    
+    const animatedStyle = useSpring({
+        // mass: 5,
+        // tension: 50,
+        // friction: 25,
+        // clamp: true,
+        opacity: toggle ? 1 : 0,
+        // transform: toggle ? 'scale(1,1)' : 'scale(1,0)',
+        config: { duration: 500,
+            // easing: easings.easeInOutElastic 
+        },
+        marginLeft: toggle ? 0 : -500,
+        // marginRight: toggle ? 0 : -500
+    });
+
+    useEffect(() => {
+        setToggle(true);
+    }, []);
+
     return (
+        <animated.div style={animatedStyle}>
         <div className='App' id='Header'>
             <Link to='/'>
                 <Container>
@@ -21,6 +44,7 @@ const Header = () => {
                 </Container>
             </Link>
         </div>
+        </animated.div>
     );
 }
 
