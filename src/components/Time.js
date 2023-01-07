ebce2b0916b274cbf3b485d463e3156aac262054
Eitan Fire <eitanfire@gmail.com>
2023-01-06 23:16:28 -0700

    delete index.html

--- src/components/Time.js
+++ src/components/Time.js
@@ -8,6 +8,7 @@ import { useState, useEffect } from 'react';
 import { useSpring, animated, } from 'react-spring';
 // import  aDayBDay  from '../utils/aDayBDay';
 import '../App.css';
+import {getCurrentDate} from "../utils/getCurrentDate";
 
 const Time = () => {
   const [toggle, setToggle] = useState(false)
@@ -22,9 +23,11 @@ useEffect(() => {
     setToggle(true);
 }, []);
 
-const brightDay = <span id='aBAB'>
-{aDayBDay.slice(1)}
-</span>
+const brightDay = (
+  <span id="aBAB">
+    {aDayBDay.slice(1)}
+  </span>
+);
 
 // const onlyAB = aDayBDay.filter(AB => AB === 'B' || 'A');
 
@@ -33,6 +36,7 @@ const brightDay = <span id='aBAB'>
       <Container className="time">
         <Row>
           <Col>
+            {/* <span>{getCurrentDate}</span> */}
             <span>Today is {weekday}.</span>
           </Col>
         </Row>
--- src/utils/aDayBDay.js
+++ src/utils/aDayBDay.js
@@ -10,6 +10,4 @@ const aDayBDay = [
         const d = new Date();
         let block = aDayBDay[d.getDay()];
 
-        // if (aDayBDay.find(AB => AB === 'B' || 'A'));
-
 export default block;
