fa6761e21b3052f49cb324e110dae32e1378c63c
Eitan Fire <eitanfire@gmail.com>
2023-05-01 23:24:02 -0600

    format AnimatedAccordion

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,34 +1,34 @@
-import React, { useState } from 'react';
-import { Card, CardBody, CardTitle } from 'reactstrap';
+// import React, { useState } from 'react';
+// import { Card, CardBody, CardTitle } from 'reactstrap';
 
-const Accordion = ({ icon, title, content, image, id }) => {
-  const [isActive, setIsActive] = useState(false);
-  return (
-    <div className="accordion-item">
-      <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
-        <CardTitle>
-          <span id='icon'>
-          {icon}
-          </span>
-          {title}
-          {image}
-          {id}
-          <span
-            className="d-none d-md-block d-xxl-none"
-            id="openCloseSymbols"
-            style={{ color: "red" }}
-          >
-            {isActive ? " 🪐" : " 🚀"}
-          </span>
-        </CardTitle>
-      </div>
-      <Card>
-        <CardBody className="accordion-content-theme">
-          {isActive && <div className="accordion-content">{content}</div>}
-        </CardBody>
-      </Card>
-    </div>
-  );
-};
+// const Accordion = ({ icon, title, content, image, id }) => {
+//   const [isActive, setIsActive] = useState(false);
+//   return (
+//     <div className="accordion-item">
+//       <div className="accordion-title" onClick={() => setIsActive(!isActive)}>
+//         <CardTitle>
+//           <span id='icon'>
+//           {icon}
+//           </span>
+//           {title}
+//           {image}
+//           {id}
+//           <span
+//             className="d-none d-md-block d-xxl-none"
+//             id="openCloseSymbols"
+//             style={{ color: "red" }}
+//           >
+//             {isActive ? " 🪐" : " 🚀"}
+//           </span>
+//         </CardTitle>
+//       </div>
+//       <Card>
+//         <CardBody className="accordion-content-theme">
+//           {isActive && <div className="accordion-content">{content}</div>}
+//         </CardBody>
+//       </Card>
+//     </div>
+//   );
+// };
 
-export default Accordion;
+// export default Accordion;
