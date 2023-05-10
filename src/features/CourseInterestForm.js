fede1e8efbe01e41df764f7eee48a1da73b1f6eb
Eitan Fire <eitanfire@gmail.com>
2023-05-09 21:33:36 -0600

    fix .classImage height

--- src/features/CourseInterestForm.js
+++ src/features/CourseInterestForm.js
@@ -1,10 +1,10 @@
 function CourseInterestForm(props) {
   const {
-    iframeSource = '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScOVlIaiTYDxpjdue7VtUMKjtLtxDF3Vvz46TZT_lVbuxsQoQ/viewform?embedded=true" width="1366" height="768" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+    iframeSource = '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScOVlIaiTYDxpjdue7VtUMKjtLtxDF3Vvz46TZT_lVbuxsQoQ/viewform?embedded=true" width="675" height="768" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
   } = props;
   return (
     <div
-      className="CourseInterestForm"
+      className="CourseInterestForm col-lg-12"
       dangerouslySetInnerHTML={{ __html: iframeSource }}
     ></div>
   );
