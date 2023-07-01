f925d860c17fb7215109aca39a7e4dbe2ab712ad
Eitan Fire <eitanfire@gmail.com>
2023-06-30 23:55:34 -0600

    format file

--- src/utils/aDayBDay.js
+++ src/utils/aDayBDay.js
@@ -1,5 +1,5 @@
 const aDayBDay = [false, "n A", " B", "n A", " B", "n A", false];
-        const d = new Date();
-        let block = aDayBDay[d.getDay()];
+const d = new Date();
+let block = aDayBDay[d.getDay()];
 
 export default block;
