f2a670d3e0d7acc4d392d3463c0e1a16a85c840b
Eitan Fire <eitanfire@gmail.com>
2023-07-18 23:50:21 -0600

    format document

--- src/features/AnimatedAccordion.js
+++ src/features/AnimatedAccordion.js
@@ -1,54 +1,53 @@
-
-import { useState, useEffect } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-import { useSpring, animated, scale, easings } from 'react-spring';
+import { useState, useEffect } from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import { useSpring, animated, scale, easings } from "react-spring";
 
 const AnimatedAccordion = ({ icon, title, content, image, id }) => {
-    const [isActive, setIsActive] = useState(false);
-    const [toggle, setToggle] = useState(false)
-    
-    const animatedStyle = useSpring({
-        opacity: toggle ? 1 : 0,
-        // transform: toggle ? 'scale(1,1)' : 'scale(1,0)',
-        easing: easings.easeOutExpo,
-        config: { duration: 500 },
-    });
+  const [isActive, setIsActive] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
-    useEffect(() => {
-        setToggle(true);
-    }, []);
+  const animatedStyle = useSpring({
+    opacity: toggle ? 1 : 0,
+    // transform: toggle ? 'scale(1,1)' : 'scale(1,0)',
+    easing: easings.easeOutExpo,
+    config: { duration: 500 },
+  });
 
-    return (
-      <animated.div style={animatedStyle}>
-        <div className="accordion-item">
-          <CardText
-            className="accordion-title"
-            onClick={() => setIsActive(!isActive)}
-          >
-            <CardTitle>
-              <span id="openCloseSymbols">{icon}</span>
-              {title}
-              {image}
-              {id}
-              <span
-                className="d-none d-md-block d-xxl-none"
-                id="openCloseSymbols"
-                // style={{ color: "red" }}
-              >
-                {isActive ? " 🪐" : " 🚀"}
-              </span>
-            </CardTitle>
-          </CardText>
-          <Card>
-            <CardBody className="accordion-content-theme">
-              {isActive && (
-                <div className="accordion-content card-xxl-12">{content}</div>
-              )}
-            </CardBody>
-          </Card>
-        </div>
-      </animated.div>
-    );
-  };
+  useEffect(() => {
+    setToggle(true);
+  }, []);
 
-  export default AnimatedAccordion;
+  return (
+    <animated.div style={animatedStyle}>
+      <div className="accordion-item">
+        <CardText
+          className="accordion-title"
+          onClick={() => setIsActive(!isActive)}
+        >
+          <CardTitle>
+            <span id="openCloseSymbols">{icon}</span>
+            {title}
+            {image}
+            {id}
+            <span
+              className="d-none d-md-block d-xxl-none"
+              id="openCloseSymbols"
+              // style={{ color: "red" }}
+            >
+              {isActive ? " 🪐" : " 🚀"}
+            </span>
+          </CardTitle>
+        </CardText>
+        <Card>
+          <CardBody className="accordion-content-theme">
+            {isActive && (
+              <div className="accordion-content card-xxl-12">{content}</div>
+            )}
+          </CardBody>
+        </Card>
+      </div>
+    </animated.div>
+  );
+};
+
+export default AnimatedAccordion;
