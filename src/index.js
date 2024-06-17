a84a21f09374f155124bcbe55c8c62717dd60e19
Eitan Fire <eitanfire@gmail.com>
2024-06-16 23:26:12 -0600

    delete comment

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
