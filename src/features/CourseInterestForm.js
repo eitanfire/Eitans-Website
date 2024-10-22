b4dbcf2fac7143bdd132de1a7eb8e3aa9a6833cd
Eitan Fire <eitanfire@gmail.com>
2024-10-21 21:34:55 -0600

    add TODO

--- src/features/CourseInterestForm.js
+++ src/features/CourseInterestForm.js
@@ -1,6 +1,8 @@
 function CourseInterestForm(props) {
   const {
-    iframeSource = '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScOVlIaiTYDxpjdue7VtUMKjtLtxDF3Vvz46TZT_lVbuxsQoQ/viewform?embedded=true" width="675" height="768" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
+    iframeSource = 
+    // TODO GET DOC 
+    '<iframe src="https://docs.google.com/forms/d/e/1FAIpQLScOVlIaiTYDxpjdue7VtUMKjtLtxDF3Vvz46TZT_lVbuxsQoQ/viewform?embedded=true" width="675" height="768" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>',
   } = props;
   return (
     <div
